b0a49e754356fd8a0acd787b93488d61d506c132
gustafl <gustafl@bahnhof.se>
2016-10-30 13:50:21 +0100

    Minor fix in AssemblyInfo.cs.

--- ConstructorInjection/Properties/AssemblyInfo.cs
+++ ConstructorInjection/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Samurais")]
+[assembly: AssemblyTitle("ConstructorInjection")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Samurais")]
+[assembly: AssemblyProduct("ConstructorInjection")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
